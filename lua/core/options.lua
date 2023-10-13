8ea799c92356fa01a19167a5efccb51dd1fb0c08
Steve White <swhite85@gmail.com>
2023-10-13 14:48:49 -0400

    Update options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,11 +4,18 @@ vim.g.maplocalleader = ' '
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
-vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.confirm = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.mouse = 'a'
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 10
 
 -- use spaces for tabs and whatnot
 vim.opt.tabstop = 2
